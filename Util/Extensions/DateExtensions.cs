6827e06c125430f35c94dd7dbaba306ea278837b
Geyson Jr <geyson.junior@sga.pucminas.br>
2018-01-19 15:36:34 -0200

    SAARA:6 - Mundando o padrão pra expression body

---
+++ Util/Extensions/DateExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Util.Extensions
+{
+	public static class DateExtensions
+	{
+		public static DateTime Add(this DateTime date, int hours = 0, int minutes = 0, int seconds = 0) => date.Add(new TimeSpan(hours, minutes, seconds));
+
+		public static DateTime WithoutMilliseconds(this DateTime date) => new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute, date.Second);
+	}
+}
---
+++ Util/Extensions/DoubleExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Util.Extensions
+{
+	public static class DoubleExtensions
+	{
+		public static DateTime TimestampToDateTime(this double value) => new DateTime(1970, 1, 1, 0, 0, 0, 0).AddMilliseconds(value);
+
+		public static double ProportionOn(this double value, double total) => (value / total);
+
+		public static double PercentageOn(this double value, double total) => value.ProportionOn(total) * 100;
+	}
+}
