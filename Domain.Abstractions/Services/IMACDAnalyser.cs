7cd371187262038e60e3e79bdb4a430baa0fce61
Ana Luiza Sanches <analuizatrz@gmail.com>
2018-01-23 20:48:50 -0200

    SAARA:3 - Pequenas Correções
    Alteração dos nomes Analiser para Analyser

---
+++ Domain.Abstractions/Services/IMACDAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Abstractions.Entitys;
+using Domain.Abstractions.Entitys.AnalisysConfig;
+using Domain.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Abstractions.Services
+{
+    public interface IMACDAnalyser
+	{
+		decimal LongEMA { get; set; }
+		decimal ShortEMA { get; set; }
+		decimal MACD { get; set; }
+		decimal Signal { get; set; }
+		decimal Histogram { get; set; }
+		IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis);
+		IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis, ICandle candle);
+	}
+}
---
+++ Domain/Services/MACDAnalyser.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Abstractions;
+using Domain.Abstractions.Entitys;
+using Domain.Abstractions.Entitys.AnalisysConfig;
+using Domain.Abstractions.Services;
+using Domain.Entitys;
+using Domain.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Services
+{
+	public class MACDAnalyser : IMACDAnalyser
+	{
+		public virtual decimal LongEMA { get; set; }
+		public virtual decimal ShortEMA { get; set; }
+		public virtual decimal MACD { get; set; }
+		public virtual decimal Signal { get; set; }
+		public virtual decimal Histogram { get; set; }
+		public MACDAnalyser()
+		{
+
+		}
+		public MACDAnalyser(IMACDConfig config, ICandleAnalyser analysis) : this()
+		{
+			Calculate(config, analysis);
+		}
+		public MACDAnalyser(IMACDConfig config, ICandleAnalyser analysis, ICandle candle) : this()
+		{
+			Calculate(config, analysis, candle);
+		}
+		public virtual IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis)
+		{
+			return Calculate(config, analysis, analysis.Main);
+		}
+		public virtual IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis, ICandle candle)
+		{
+			LongEMA = analysis.EMA(config.LongEMA);
+			ShortEMA = analysis.EMA(config.ShortEMA);
+			MACD = ShortEMA - LongEMA;
+			Signal = analysis
+							.Previous
+							.TakePrevious(candle, config.SignalEMA)
+							.Select(c => new MACDAnalyser(config, analysis, c).MACD)
+							.ToList()
+							.EMA(config.SignalEMA);
+
+			Histogram = MACD - Signal;
+
+			return this;
+		}
+	}
+}
