f2c8bd31a802f2a1ee155f30d72b5fb981c2656c
Ana Luiza Sanches <analuizatrz@gmail.com>
2018-02-08 18:05:15 -0200

    SAARA:38 - testes enumerable extensions, indexOfMax IndexOfMin

--- Util.Test/Extensions/EnumerableExtensions.Test.cs
+++ Util.Test/Extensions/EnumerableExtensions.Test.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Util.Extensions;
 using Xunit;
@@ -72,6 +73,92 @@ namespace Infra.Test
 		{
 			var actual = values.ToList().SkipAndTake(index, length);
 			Assert.True(expected.SequenceEqual(actual));
+		}
+
+		[
+			Theory(DisplayName = "The object returned from with max should be"),
+			InlineData(0, new long[] {}),
+			InlineData(0, new long[] { 9, 9, 9 }),
+			InlineData(2, new long[] { 1, 2, 3 }),
+			InlineData(1, new long[] { 2, 3, 1 }),
+			InlineData(0, new long[] { 3, 1, 2 }),
+			InlineData(10, new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }),
+			InlineData(9, new long[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 10, 10 }),
+			InlineData(0, new long[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }),
+			InlineData(0, new long[] { 10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1 }),
+			InlineData(3, new long[] { 3, 7, 1, 10, 4, 5, 9, 0, 2, 6, 8 })
+		]
+		public void The_object_returned_from_with_max_should_be(int expected, long[] values)
+		{
+			var actual = WithId(values).WithMax(v => v.value).id;
+			Assert.Equal(expected, actual);
+		}
+
+		[
+			Theory(DisplayName = "The object returned from with min should be"),
+			InlineData(0, new long[] { }),
+			InlineData(0, new long[] { 9, 9, 9 }),
+			InlineData(0, new long[] { 1, 2, 3 }),
+			InlineData(2, new long[] { 2, 3, 1 }),
+			InlineData(1, new long[] { 3, 1, 2 }),
+			InlineData(0, new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }),
+			InlineData(0, new long[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 10, 10 }),
+			InlineData(10, new long[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }),
+			InlineData(9, new long[] { 10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1 }),
+			InlineData(7, new long[] { 3, 7, 1, 10, 4, 5, 9, 0, 2, 6, 8 })
+		]
+		public void The_object_returned_from_with_min_should_be(int expected, long[] values)
+		{
+			var actual = WithId(values).WithMin(v => v.value).id;
+			Assert.Equal(expected, actual);
+		}
+
+		[
+			Theory(DisplayName = "The index of max returned should be"),
+			InlineData(-1, new long[] { }),
+			InlineData(0, new long[] { 9, 9, 9 }),
+			InlineData(2, new long[] { 1, 2, 3 }),
+			InlineData(1, new long[] { 2, 3, 1 }),
+			InlineData(0, new long[] { 3, 1, 2 }),
+			InlineData(10, new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }),
+			InlineData(9, new long[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 10, 10 }),
+			InlineData(0, new long[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }),
+			InlineData(0, new long[] { 10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1 }),
+			InlineData(3, new long[] { 3, 7, 1, 10, 4, 5, 9, 0, 2, 6, 8 })
+		]
+		public void The_index_of_max_returned_should_be(int expected, long[] values)
+		{
+			var actual = values.IndexOfMax(v => v);
+			Assert.Equal(expected, actual);
+		}
+
+		[
+			Theory(DisplayName = "The index of min returned should be"),
+			InlineData(-1, new long[] { }),
+			InlineData(0, new long[] { 9, 9, 9 }),
+			InlineData(0, new long[] { 1, 2, 3 }),
+			InlineData(2, new long[] { 2, 3, 1 }),
+			InlineData(1, new long[] { 3, 1, 2 }),
+			InlineData(0, new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }),
+			InlineData(0, new long[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 10, 10 }),
+			InlineData(10, new long[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }),
+			InlineData(9, new long[] { 10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1 }),
+			InlineData(7, new long[] { 3, 7, 1, 10, 4, 5, 9, 0, 2, 6, 8 })
+		]
+		public void The_index_of_min_returned_should_be(int expected, long[] values)
+		{
+			var actual = values.IndexOfMin(v => v);
+			Assert.Equal(expected, actual);
+		}
+
+		public IList<(int id, T value)> WithId<T>(IList<T> values)
+		{
+			var result = new List<(int id, T value)>();
+			for (int i = 0; i < values.Count; i++)
+			{
+				result.Add((i, values[i]));
+			}
+			return result;
 		}
 	}
 }
