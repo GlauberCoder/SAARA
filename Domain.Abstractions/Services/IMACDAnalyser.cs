8602256a7ece80c4a6ec01029730de013b09e626
Ana Luiza Sanches <analuizatrz@gmail.com>
2018-02-20 19:38:32 -0300

    SAARA:26 - documentacao MACD Analyser

--- Domain.Abstractions/Services/IMACDAnalyser.cs
+++ Domain.Abstractions/Services/IMACDAnalyser.cs
@@ -5,13 +5,28 @@ using System.Collections.Generic;
 
 namespace Domain.Abstractions.Services
 {
+	/// <summary>
+	/// MACD Analyser provides tools to explore MACD oscillator, by calculating trend and trade signals
+	/// </summary>
 	public interface IMACDAnalyser
 	{
 		IMACD MACD { get; }
 		IList<IMACD> MACDs { get; }
+		/// <summary>
+		/// Trend based on actual MACD and Signal
+		/// </summary>
 		Trend Trend { get; set; }
+		/// <summary>
+		/// Trade signal based on MACD and Signal, given that there was a cross
+		/// </summary>
 		TradeSignal CrossSignal { get; set; }
+		/// <summary>
+		/// Trade based on MACD, given that there was a cross between MACD and line 0
+		/// </summary>
 		TradeSignal CenterCrossSignal { get; set; }
+		/// <summary>
+		/// Trend based on comparing the trend of MACD and the trend of candle's closing price
+		/// </summary>
 		Trend DivergenceSignal { get; set; }
 		IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis);
 		IMACDAnalyser Calculate(IMACDConfig config, ICandleAnalyser analysis, ICandle candle);
