548ee884222164aec28f50cdd93fc1249aaf6e38
Ana Luiza Sanches <analuizatrz@gmail.com>
2018-02-23 16:31:59 -0300

    SAARA:27 - Documentando EMA

--- Domain.Abstractions/Services/IEMAAnalyser.cs
+++ Domain.Abstractions/Services/IEMAAnalyser.cs
@@ -2,9 +2,19 @@
 
 namespace Domain.Abstractions.Services
 {
+	/// <summary>
+	///  Exponential Moving Averages smooth the price data to form a trend following indicator, defining the current direction with a lag. It can be used to identify the direction of the trend or define potential support and resistance levels.
+	/// http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:moving_averages
+	/// </summary>
 	public interface IEMAAnalyser
 	{
+		/// <summary>
+		/// Short-term Exponential Moving Average
+		/// </summary>
 		decimal ShortEMA { get; }
+		/// <summary>
+		/// Long-term Exponential Moving Average
+		/// </summary>
 		decimal LongEMA { get; }
 		IEMAAnalyser Calculate(IEMAConfig config, ICandleAnalyser analysis);
 	}
