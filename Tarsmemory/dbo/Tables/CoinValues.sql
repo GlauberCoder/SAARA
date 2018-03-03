79e80aaabddee007bbcdbfec53ef6f853cc6e821
Unknown <glauber.fdim@gmail.com>
2018-03-03 00:04:45 -0300

    Organizando projeto

---
+++ Tarsmemory/dbo/Tables/CoinValues.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CoinValues] (
+    [ID]         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Date]       DATETIME       NOT NULL,
+    [PriceOpen]  FLOAT (53)     NOT NULL,
+    [PriceClose] FLOAT (53)     NOT NULL,
+    [Amount]     FLOAT (53)     NOT NULL,
+    [Action]     INT            NULL,
+    [Exchanger]  NVARCHAR (MAX) NULL
+);
+
---
+++ Tarsmemory/dbo/Tables/OrderBookAndTradesAnalysis.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[OrderBookAndTradesAnalysis] (
+    [Id]          INT                IDENTITY (1, 1) NOT NULL,
+    [Timestamp]   DATETIMEOFFSET (7) NOT NULL,
+    [Volume]      FLOAT (53)         NOT NULL,
+    [Open]        FLOAT (53)         NOT NULL,
+    [Close]       FLOAT (53)         NOT NULL,
+    [High]        FLOAT (53)         NOT NULL,
+    [Low]         FLOAT (53)         NOT NULL,
+    [Time_Window] INT                NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
